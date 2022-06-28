c380d498edfde7653f297b20a6441d503b31e00f
LaM0uette <lam0uette.dev@gmail.com>
2022-06-28 11:41:38 +0200

    add ico

--- GoMap.go
+++ GoMap.go
@@ -1,8 +1,11 @@
 //go:generate goversioninfo -icon=GoMap.ico -manifest=GoMap.exe.manifest
 package main
 
+import "fmt"
+
 func main() {
 
 	//f := "C:\\Users\\XD5965\\OneDrive - EQUANS\\Bureau\\DLG\\ERREURS-REC-DLG-40-SAUB-NISY-01-V12.xlsx"
+	fmt.Println("test")
 
 }
