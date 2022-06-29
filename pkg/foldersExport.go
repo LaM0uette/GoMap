bf6f71dee5e88c8cc4bcc716bcadf2340edb9050
LaM0uette <lam0uette.dev@gmail.com>
2022-06-29 14:04:55 +0200

    ajout de la création des dossiers dlg

--- pkg/foldersExport.go
+++ pkg/foldersExport.go
@@ -3,6 +3,7 @@ package pkg
 import (
 	"GoMap/coSql"
 	"fmt"
+	"path"
 	"strings"
 )
 
@@ -11,18 +12,24 @@ func FoldersExport() {
 	DrawStart("Folders Export")
 	DrawSep("INITIALISATION")
 
-	//nro := GetUserInput("Entrez le NRO : ")
-	//sro := GetUserInput("Entrez le SRO : ")
 	refcode3 := strings.ToUpper(GetUserInput("Entrez le REFCODE3 : "))
 	phase := strings.ToUpper(GetUserInput("Entrez la PHASE : "))
 	nLivaison := GetUserInput("Entrez le n° de LIVRAISON : ")
 	nVersion := GetUserInput("Entrez le n° de VERSION : ")
+	rc := coSql.GetRefcodeData("BIMA")
 
-	fmt.Println(refcode3, phase, nLivaison, nVersion)
+	folderDLG := path.Join(
+		FolderExportsGrace,
+		fmt.Sprintf("RIP%v", rc.RefCode1),
+		fmt.Sprintf("NRO%v_PM%v_%s", rc.NRO, rc.SRO, refcode3),
+		fmt.Sprintf("%s_%v", phase, nLivaison),
+		fmt.Sprintf("V%v", nVersion))
 
-	coSql.GetRefcodeData("BIMA")
-
-	//folderDlg := fmt.Sprintf("")
-	//CreateNewFolder(path.Join(FolderExportsGrace, folderDlg))
-
+	CreateNewFolder(folderDLG)
+	CreateNewFolder(path.Join(folderDLG, "__Historiques__"))
+	CreateNewFolder(path.Join(folderDLG, "DJANGO"))
+	CreateNewFolder(path.Join(folderDLG, "DLG"))
+	CreateNewFolder(path.Join(folderDLG, "DLG", ""))
+	CreateNewFolder(path.Join(folderDLG, "PDB"))
+	CreateNewFolder(path.Join(folderDLG, "ROPT"))
 }
