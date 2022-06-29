1998038deeda0ed8021a2057682d2b89ee119527
LaM0uette <lam0uette.dev@gmail.com>
2022-06-29 15:44:32 +0200

    create dlg folder in function

--- pkg/foldersExport.go
+++ pkg/foldersExport.go
@@ -25,21 +25,8 @@ func FoldersExport() {
 	DrawSep("INITIALISATION")
 
 	dlg := getDlgData()
+	createDlgFolders(dlg)
 
-	folderDLG := path.Join(
-		FolderExportsGrace,
-		fmt.Sprintf("RIP%v", dlg.Refcode1),
-		fmt.Sprintf("NRO%v_PM%v_%s", dlg.NRO, dlg.SRO, dlg.Refcode3),
-		fmt.Sprintf("%s_%v", dlg.Phase, dlg.Livaison),
-		fmt.Sprintf("V%v", dlg.Version))
-
-	CreateNewFolder(folderDLG)
-	CreateNewFolder(path.Join(folderDLG, "__Historiques__"))
-	CreateNewFolder(path.Join(folderDLG, "DJANGO"))
-	CreateNewFolder(path.Join(folderDLG, "DLG"))
-	CreateNewFolder(path.Join(folderDLG, "DLG", fmt.Sprintf("%s-DLG-%v-%s-%s-%s-V%v", dlg.Phase[0:3], dlg.Refcode1, dlg.Refcode2, dlg.Refcode3, fmt.Sprintf("%02d", dlg.Livaison), dlg.Version)))
-	CreateNewFolder(path.Join(folderDLG, "PDB"))
-	CreateNewFolder(path.Join(folderDLG, "ROPT"))
 }
 
 func getDlgData() dlgData {
@@ -60,4 +47,28 @@ func getDlgData() dlgData {
 		Livaison: livaison,
 		Version:  _version,
 	}
+}
+
+func createDlgFolders(dlg dlgData) {
+	fRIP := fmt.Sprintf("RIP%v", dlg.Refcode1)
+	fZone := fmt.Sprintf("NRO%v_PM%v_%s", dlg.NRO, dlg.SRO, dlg.Refcode3)
+	fLivraison := fmt.Sprintf("%s_%v", dlg.Phase, dlg.Livaison)
+	fVersion := fmt.Sprintf("V%v", dlg.Version)
+
+	p := path.Join(FolderExportsGrace, fRIP, fZone, fLivraison, fVersion)
+	folders := []string{"__Historiques__", "DJANGO", "DLG", "PDB", "ROPT"}
+
+	for folder := range folders {
+		CreateNewFolder(path.Join(p, folders[folder]))
+	}
+
+	CreateNewFolder(path.Join(p,
+		"DLG",
+		fmt.Sprintf("%s-DLG-%v-%s-%s-%s-V%v",
+			dlg.Phase[0:3],
+			dlg.Refcode1,
+			dlg.Refcode2,
+			dlg.Refcode3,
+			fmt.Sprintf("%02d", dlg.Livaison),
+			dlg.Version)))
 }
