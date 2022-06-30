af2c60f25ab1377ad4a6d7905b9eaf4cb4f5b3ae
LaM0uette <lam0uette.dev@gmail.com>
2022-06-30 08:57:29 +0200

    suppr zip

--- pkg/cleanDlgFiles.go
+++ pkg/cleanDlgFiles.go
@@ -1,19 +1,8 @@
 package pkg
 
-import (
-	"fmt"
-)
-
 func CleanDlgFiles() {
 
 	DrawStart("Clean DLG Files")
 	DrawSep("RUN")
 
-	files := []string{"pkg"}
-	output := "done.zip"
-
-	if err := ZipFiles(output, files); err != nil {
-		panic(err)
-	}
-	fmt.Println("Zipped File:", output)
 }
